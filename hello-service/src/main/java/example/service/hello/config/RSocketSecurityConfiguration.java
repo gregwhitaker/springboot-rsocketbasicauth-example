7c595cadcfc8cd8130fbeabf3aff42857dc63395
gregwhitaker <gwhitake@gmail.com>
2019-12-17 16:31:52 -0800

    Added security config

---
+++ hello-service/src/main/java/example/service/hello/config/RSocketSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package example.service.hello.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+public class RSocketSecurityConfiguration {
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+}
