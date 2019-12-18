bf3ba0b5a0c89ed58da58bc100b278226c944880
gregwhitaker <gwhitake@gmail.com>
2019-12-18 10:32:17 -0800

    Doing work

--- hello-client/src/main/java/example/client/hello/HelloClientApplication.java
+++ hello-client/src/main/java/example/client/hello/HelloClientApplication.java
@@ -2,9 +2,11 @@ package example.client.hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
 
 import static picocli.CommandLine.Option;
@@ -25,6 +27,9 @@ public class HelloClientApplication {
     @Component
     public class Runner implements CommandLineRunner {
 
+        @Autowired
+        private RSocketRequester helloServiceRequester;
+
         @Override
         public void run(String... args) throws Exception {
             ClientArguments params = populateCommand(new ClientArguments(), args);
@@ -33,6 +38,13 @@ public class HelloClientApplication {
             LOG.debug("password: {}", params.password);
             LOG.debug("method: {}", params.method);
             LOG.debug("name: {}", params.name);
+
+            // Sending request to the hello-service
+            String message = helloServiceRequester.route(params.method)
+                    .retrieveMono(String.class)
+                    .block();
+
+            LOG.info("Response: {}", message);
         }
     }
 
---
+++ hello-client/src/main/java/example/client/hello/config/RSocketConfiguration.java
@@ -0,0 +1,27 @@
+package example.client.hello.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeTypeUtils;
+
+@Configuration
+public class RSocketConfiguration {
+
+    @Value("${example.service.hello.hostname")
+    private String helloServiceHostname;
+
+    @Value("${example.service.hello.port")
+    private int helloServicePort;
+
+    @Bean
+    public RSocketRequester helloServiceRequester(RSocketStrategies rSocketStrategies) {
+        return RSocketRequester.builder()
+                .rsocketStrategies(rSocketStrategies)
+                .dataMimeType(MimeTypeUtils.TEXT_PLAIN)
+                .connectTcp(helloServiceHostname, helloServicePort)
+                .block();
+    }
+}
