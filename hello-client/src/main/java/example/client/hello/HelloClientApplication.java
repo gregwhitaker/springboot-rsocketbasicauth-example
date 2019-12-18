1d61a4e920d0aa9bcf0342a3e95599dc8c4b7b1f
gregwhitaker <gwhitake@gmail.com>
2019-12-18 10:14:59 -0800

    Doing work

--- hello-client/src/main/java/example/client/hello/HelloClientApplication.java
+++ hello-client/src/main/java/example/client/hello/HelloClientApplication.java
@@ -1,5 +1,51 @@
 package example.client.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+import static picocli.CommandLine.Option;
+import static picocli.CommandLine.Parameters;
+import static picocli.CommandLine.populateCommand;
+
+@SpringBootApplication
 public class HelloClientApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloClientApplication.class);
+
+    public static void main(String... args) {
+        SpringApplication.run(HelloClientApplication.class, args);
+    }
+
+    /**
+     * Runs the application.
+     */
+    @Component
+    public class Runner implements CommandLineRunner {
+
+        @Override
+        public void run(String... args) throws Exception {
+            ClientArguments params = populateCommand(new ClientArguments(), args);
+        }
+    }
+
+    /**
+     * Hello client command line arguments.
+     */
+    public static class ClientArguments {
+
+        @Option(names = "username")
+        public String username;
+
+        @Option(names = "password")
+        public String password;
+
+        @Parameters(index = "0")
+        public String method;
+
+        @Parameters(index = "1")
+        public String name;
+    }
 }
