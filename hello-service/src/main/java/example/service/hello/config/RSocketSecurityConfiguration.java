a6c81e76659ece600cd53ba27aa211fb2273e37a
gregwhitaker <gwhitake@gmail.com>
2019-12-18 09:13:17 -0800

    Doing work

--- hello-service/src/main/java/example/service/hello/config/RSocketSecurityConfiguration.java
+++ hello-service/src/main/java/example/service/hello/config/RSocketSecurityConfiguration.java
@@ -2,10 +2,13 @@ package example.service.hello.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
 
 @Configuration
 @EnableRSocketSecurity
@@ -20,5 +23,17 @@ public class RSocketSecurityConfiguration {
                 .build();
 
         return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public PayloadSocketAcceptorInterceptor rsocketInterceptor(RSocketSecurity rsocket) {
+        rsocket.authorizePayload(authorize ->
+                        authorize
+                                .anyRequest().authenticated()
+                                .anyExchange().permitAll()
+                )
+                .basicAuthentication(Customizer.withDefaults());
+
+        return rsocket.build();
     }
 }
